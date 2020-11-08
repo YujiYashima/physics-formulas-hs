b70b0506179c031ea4128a59a579d8077f35d81a
YujiYashima <pedro.yashima@etec.sp.gov.br>
2020-11-08 03:29:31 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 function velocidadeMedia(distancia, tempo, unidadeMedida) {
-
     return `${distancia/tempo} ${unidadeMedida}`  
-
 }
-
-const vel = (d, t, u) => `${d/t} ${u}`
 
 module.exports = {
-    velocidadeMedia,
-    vel
+    velocidadeMedia
 }
-
-console.log(vel(10, 1, "m/s"))
