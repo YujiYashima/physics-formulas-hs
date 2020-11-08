e9c4854baf568a35d1b43f974d366a4a7cdb99cf
YujiYashima <pedro.yashima@etec.sp.gov.br>
2020-11-08 03:13:48 -0300

    Tests in npm package

--- teste.ts
+++
@@ -1,6 +0,0 @@
-function velocidadeMedia(distancia: number, tempo: number, unidadeMedida: string) {
-
-    return `${distancia/tempo} ${unidadeMedida}` 
-}
-
-console.log(velocidadeMedia(12, 1, "km/h"))
