566bdb49a7ec84086a1cf7976d2a7e46a1588725
YujiYashima <pedro.yashima@etec.sp.gov.br>
2020-12-03 23:33:28 -0300

    Initiation with typescript

---
+++ dist/index.d.ts
@@ -0,0 +1,12 @@
+declare type unidadeVelocidade = "km/h" | "m/s";
+export declare const velocidadeEscalarMedia: (deslocamento: number, tempo: number, unidadeMedida: unidadeVelocidade) => string;
+export declare const converterVelocidadeMedia: (velocidade: number, converterPara: unidadeVelocidade) => string;
+export declare const funçaoHorariaDasPosicoes: (espaçoInicial: number, velocidade: number, tempo: number) => number | "Nao existe tempo negativo!";
+export declare const velocidadeRelativaEncontro: (velocidadeA: number, velocidadeB: number, unidadeMedida: unidadeVelocidade) => string;
+export declare const velocidadeRelativaPerseguiçao: (velocidadeA: number, velocidadeB: number, unidadeMedida: unidadeVelocidade) => string;
+export declare const velocidadeRelativaAfastamento: (velocidadeA: number, velocidadeB: number, unidadeMedida: unidadeVelocidade) => string;
+export declare const aceleraçaoMedia: (variacaoVelocidade: number, tempo: number) => string;
+export declare const funçaoHorariaDaVelocidade: (velocidadeInicial: number, aceleraçao: number, tempo: number) => string;
+export declare const funçaoHorariaDoEspaço: (espaçoInicial: number, velocidadeInicial: number, tempo: number, aceleraçao: number) => string;
+export declare const velocidadeMediaMUV: (velocidadeA: number, velocidadeB: number, unidadeMedida: unidadeVelocidade, print?: "console" | undefined) => string;
+export {};
